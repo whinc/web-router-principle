a86cfa7577a9f22074f2e8a27df67af2f379a505
whincwu <whincwu@163.com>
2019-05-11 18:32:51 +0800

    feat: 增加原生路由实现示例

---
+++ vanilla/hash/index.js
@@ -0,0 +1,26 @@
+// 页面加载完不会触发 hashchange，这里主动触发一次 hashchange 事件
+window.addEventListener('DOMContentLoaded', onLoad)
+// 监听路由变化
+window.addEventListener('hashchange', onHashChange)
+
+// 路由视图
+var routerView = null
+
+function onLoad () {
+  routerView = document.querySelector('.vanilla_hash .container')
+  onHashChange()
+}
+
+// 路由变化时，根据路由渲染对应 UI
+function onHashChange () {
+  switch (location.hash) {
+    case '#/home':
+      routerView.innerHTML = '<h2>Home</h2>'
+      return
+    case '#/about':
+      routerView.innerHTML = '<h2>About</h2>'
+      return
+    default:
+      return
+  }
+}
---
+++ vanilla/history/index.js
@@ -0,0 +1,34 @@
+// 页面加载完不会触发 hashchange，这里主动触发一次 hashchange 事件
+window.addEventListener('DOMContentLoaded', onLoad)
+// 监听路由变化
+window.addEventListener('popstate', onPopState)
+
+// 路由视图
+var routerView = null
+
+function onLoad () {
+  routerView = document.querySelector('.vanilla_history .container')
+  onPopState()
+
+  // 拦截 <a> 标签点击事件默认行为， 点击时使用 pushState 修改 URL并更新手动 UI，从而实现点击链接更新 URL 和 UI 的效果。
+  var linkList = document.querySelectorAll('.vanilla_history a[href]')
+  linkList.forEach(el => el.addEventListener('click', function (e) {
+    e.preventDefault()
+    history.pushState(null, '', el.getAttribute('href'))
+    onPopState()
+  }))
+}
+
+// 路由变化时，根据路由渲染对应 UI
+function onPopState () {
+  switch (location.pathname) {
+    case '/home':
+      routerView.innerHTML = '<h2>Home</h2>'
+      return
+    case '/about':
+      routerView.innerHTML = '<h2>About</h2>'
+      return
+    default:
+      return
+  }
+}
